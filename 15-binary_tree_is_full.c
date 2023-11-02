f3922195299932ec2fb66503483a3b522b3874e8
Katlego1993 <ledwaba.katlego.kl@gmail.com>
2023-11-02 01:19:46 -0700

    all

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int l = 0, r = 0;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
 	{
@@ -15,17 +15,17 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (tree->l && tree->r)
+		if (tree->left && tree->right)
 		{
-			l = binary_tree_is_full(tree->l);
-			r = binary_tree_is_full(tree->r);
-			if (l == 0 || r == 0)
+			left = binary_tree_is_full(tree->left);
+			right = binary_tree_is_full(tree->right);
+			if (left == 0 || right == 0)
 			{
 				return (0);
 			}
 			return (1);
 		}
-		else if (!tree->l && !tree->r)
+		else if (!tree->left && !tree->right)
 		{
 			return (1);
 		}
